51fad01338899f1a36af3d10370cedc8ebbb43b0
Nadiar AS <codxse@gmail.com>
2016-01-24 22:35:24 +0700

    I don't know what I did. Just fooling around and learning basic concept.
    This is the first time I understand to pass multiple argument with
    single variable. Yup, using `[& args]`.

--- src/fclojure_restart/elementary.clj
+++ src/fclojure_restart/elementary.clj
@@ -16,3 +16,26 @@
   [x]
   (= (- 10 (* 2 3)) x))
 
+;; Problem 3
+;; Elementary
+;; Intro to Strings
+
+(defn no3
+  [x]
+  (= x (.toUpperCase "hello world")))
+
+;; Problem 4
+;; Elementary
+;; Intro to Lists
+
+(defn no4
+  [& args]
+  into '() args)
+
+;; Problem 5
+;; Elementary
+;; List: conj
+
+(defn no5
+  [x]
+  (= x (or (conj '(2 3 4) 1) (conj '(3 4) 2 1))))
