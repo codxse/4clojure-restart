c4fa7c533f52f4ff0a2a27364efd43e65b56b9f1
Nadiar AS <codxse@gmail.com>
2016-01-24 10:28:11 +0700

    just started elementary level problem

---
+++ src/fclojure_restart/elementary.clj
@@ -0,0 +1,18 @@
+(ns fclojure-restart.elementary)
+
+;; Problem 1
+;; Elementary
+;; Nothing but the Truth
+
+(defn no1
+  [x]
+  (= true x))
+
+;; Problem 2
+;; Elementary
+;; Simple Math
+
+(defn no2
+  [x]
+  (= (- 10 (* 2 3)) x))
+
